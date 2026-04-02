52d172bb05ac670166cf9c712db97b1cfb99f9fa
Dawit-Sh <civiliandavid0@gmail.com>
2026-04-03 01:33:33 +0300

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dawit-sh.github.io',
+  base: '/-x.x',
   vite: {
     plugins: [tailwindcss()]
   }
